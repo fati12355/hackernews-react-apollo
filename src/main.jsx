f111fa25f6defecd4c0bb881f0dbf343dfc8b188
fati12355 <133292699+fati12355@users.noreply.github.com>
2025-07-16 11:24:02 -0400

    Added tachyons CSS via CDN and reorganized project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './components/App.jsx';
+import './styles/index.css';
 import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
